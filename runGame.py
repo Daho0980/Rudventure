7a75db9d14770c1f47efd661d0d194a8684aecb3
Daho0980 <0980daho@gmail.com>
2023-02-15 18:44:52 +0900

    코드 구경333

--- runGame.py
+++ runGame.py
@@ -6,11 +6,6 @@ if os.name == "posix": s = '/'
 else: s = '\\'
 
 TFP = f"{Path.cwd()}{s}"
-# Path(f'Packages{s}main.py').resolve()
 print(f"It called by {Path('pathlibTest.py').resolve()}")
 
-try: exec(open(f'{TFP}Packages{s}main.py').read())
-except:
-    try: exec(open(f'Packages{s}main.py').read())
-    except: sys.exit()
-    else: sys.exit()
+exec(open(Path(f'Packages{s}main.py').resolve()).read())
