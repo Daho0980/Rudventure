aa098372060b371fc755e2c992fdc984c514f36d
Daho0980 <0980daho@gmail.com>
2023-02-21 20:30:27 +0900

    잡기능 추가

--- Packages/modules/Textbox.py
+++ Packages/modules/Textbox.py
@@ -4,7 +4,9 @@ import re
 def escapeAnsi(line):
     ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
     return ansi_escape.sub('', line)
-
+# ┌─┐
+# │ │
+# └─┘
 def checkActualLen(line):
     Len = 0
     for i in line:
@@ -12,10 +14,29 @@ def checkActualLen(line):
         else: Len += 1
     return Len
 
-def TextBox(Inp, Type="left", maxLine=100, fillChar=" ", inDistance=0, outDistance=0, AMLS=False, endLineBreak=False):
+def TextBox(Inp, Type="left", maxLine=100, fillChar=" ", inDistance=0, outDistance=0, AMLS=False, endLineBreak=False, LineType="normal"):
         Display  = ""
 
-        Line       = {0:["╔", "╗"], 1:["╚", "╝"], 2:["╠", "╣"]}
+        Line       = {
+                     "normal":{
+                              0:["┌", "┐"],
+                              1:["└", "┘"],
+                              2:["├", "┤"],
+                              3:["─", "│"]
+                              },
+                     "double":{
+                              0:["╔", "╗"],
+                              1:["╚", "╝"],
+                              2:["╠", "╣"],
+                              3:["═", "║"]
+                              },
+                     "bold":{
+                              0:["┏", "┓"],
+                              1:["┗", "┛"],
+                              2:["┣", "┫"],
+                              3:["━", "┃"],
+                     }
+                     }
         Texts      = Inp.split("\n")
         FrontSpace = ""
         BackSpace  = ""
@@ -26,11 +47,11 @@ def TextBox(Inp, Type="left", maxLine=100, fillChar=" ", inDistance=0, outDistan
                 if maxLine < checkActualLen(escapeAnsi(i)): maxLine = checkActualLen(escapeAnsi(i))
 
         Display += "\n"*outDistance
-        Display += Line[0][0]+"═"*(maxLine)+Line[0][1]+"\n"
-        Display += ("║"+fillChar*maxLine+"║"+"\n")*inDistance
+        Display += Line[LineType][0][0]+Line[LineType][3][0]*(maxLine)+Line[LineType][0][1]+"\n"
+        Display += (Line[LineType][3][1]+fillChar*maxLine+Line[LineType][3][1]+"\n")*inDistance
         for i in range(len(Texts)):
             space = 0
-            if Texts[i] == "TextBox.Line": Display += Line[2][0]+"═"*(maxLine)+Line[2][1]+"\n"
+            if Texts[i] == "TextBox.Line": Display += Line[LineType][2][0]+Line[LineType][3][0]*(maxLine)+Line[LineType][2][1]+"\n"
             else:
                 for j in range(len(escapeAnsi(Texts[i]))):
                     if unicodedata.east_asian_width(escapeAnsi(Texts[i])[j]) in ['F', 'W']: space += 2
@@ -39,10 +60,10 @@ def TextBox(Inp, Type="left", maxLine=100, fillChar=" ", inDistance=0, outDistan
                 elif Type == "middle":
                     FrontSpace = fillChar*int((maxLine-space)/2)
                     BackSpace  = fillChar*(int((maxLine-space)/2) if (maxLine-space)%2 == 0 else int((maxLine-space)/2)+1)
-                elif Type == "right": FrontSpace = fillChar*(maxLine-space)
-                Display += ("║"+FrontSpace)+Texts[i]+(BackSpace+"║")+"\n"
-        Display += ("║"+fillChar*maxLine+"║"+"\n")*inDistance
-        Display += Line[1][0]+"═"*(maxLine)+Line[1][1]+endLine
+                elif Type == "right" : FrontSpace = fillChar*(maxLine-space)
+                Display += (Line[LineType][3][1]+FrontSpace)+Texts[i]+(BackSpace+Line[LineType][3][1])+"\n"
+        Display += (Line[LineType][3][1]+fillChar*maxLine+Line[LineType][3][1]+"\n")*inDistance
+        Display += Line[LineType][1][0]+Line[LineType][3][0]*(maxLine)+Line[LineType][1][1]+endLine
         Display += "\n"*outDistance
         return Display
 
@@ -52,4 +73,6 @@ def TextBox(Inp, Type="left", maxLine=100, fillChar=" ", inDistance=0, outDistan
 # print(TextBox("오른쪽", Type='right'))
 # print(TextBox("최대 길이 자동 설정\n아래에 짧은 줄이 있든\n\n존나 긴 줄이 있든 알아서 정해줌\n\n(현재: Type=middle, AMLS=True)", Type="middle", AMLS=True))
 # print(TextBox("글상자 안/밖 거리 설정도 가능", inDistance=5, outDistance=3, AMLS=True, Type="middle"))
-# print(TextBox("\033[1m   W a !   S a n s ! !   \033[0m", Type="middle", inDistance=1, outDistance=2, AMLS=True))
+print(TextBox("일반"))
+print(TextBox("볼드", LineType="bold"))
+print(TextBox("더블", LineType='double'))
