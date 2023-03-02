792d66fbccbec89b3631e23a09d3685d83951489
Daho0980 <0980daho@gmail.com>
2023-03-02 18:33:52 +0900

    Feat(main, mainSettings, enemy, player) : 세부 로그 추가 및 main 코드 변경
    • 게임 시작 시 이름을 추가하는 슬롯을 추가
    • player 코드에서 벽에 박거나 말랑이를 만졌을 때 또는 hp가 부족할 때
    상태를 알리는 로그를 출력하도록 수정
    • main 코드 중 게임 초기 설정을 mainSettings.py로 따로 분리

--- Packages/main.py
+++ Packages/main.py
@@ -6,10 +6,8 @@ else:               s = '/'
 TFP = f'{os.getcwd()}{s}'
 try:    exec(open(f'{TFP}Packages{s}downloadModules.py').read())
 except: exec(open(f'{TFP}Packages{s}downloadModules.py').read())
-from   pynput.keyboard              import Key, Listener
-import Packages.options             as     options
 import Packages.globalFunctions     as     globalFunctions
-from   Packages.modules             import player, rooms, status, stages, quests, selector, Textbox, logger, makeNewListener
+from   Packages.modules             import player, rooms, status, stages, quests, selector, Textbox, logger, makeNewListener, mainSettings
 from   Packages.globalFunctions     import clear
 
 p, r, s, S, S1, gbf, t, mnl= player.player, rooms, status, stages.stages, stages, globalFunctions, Textbox, makeNewListener
@@ -19,7 +17,9 @@ p.set()
 
 
 def gameChecker():
-    if int((s.hp / s.Mhp) * 100) <= 30 and s.hpLow == False: s.hpLow = True; gbf.play(f"{s.TFP}sounds{s.s}hp_low.wav")
+    if int((s.hp / s.Mhp) * 100) <= 30 and s.hpLow == False:
+        s.hpLow = True
+        gbf.play(f"{s.TFP}sounds{s.s}hp_low.wav"); logger.addLog(f"{s.colors['lY']}{s.name}{s.colors['end']} 님의 체력이 부족합니다! {s.colors['R']}(현재 체력 : {s.hp}){s.colors['end']}")
     elif int((s.hp / s.Mhp) * 100) > 30: s.hpLow = False
 
     victory = quests.quest(s.stage)
@@ -58,20 +58,7 @@ def gameChecker():
         sys.exit()
 
 
-try: gbf.play(f'{os.getcwd()}{s.s}Packages{s.s}sounds{s.s}smash.wav')
-except: s.TFP = f'{s.s}'; gbf.play(f'{os.getcwd()}{s.s}Packages{s.s}sounds{s.s}smash.wav')
-if s.frame == 0:
-    selectFrame = selector.selector.Dropdown(f'{s.LOGO}를 시작하기 전에, 프레임을 설정해주세요', {'1프레임':'컨트롤을 포기하겠다는 의지가 느껴집니다.', '30프레임 (권장)':'위쪽 터미널 바가 덜 깜빡거립니다.', '60프레임':'위쪽 터미널 바가 더 깜빡거립니다.'}, [1,0,255,10], '@')
-    frames = [1, 30, 60]; s.frame = frames[selectFrame-1]; gbf.play(f'{s.TFP}sounds{s.s}smash.wav')
-gbf.play(f'{s.TFP}sounds{s.s}crack.wav')
-print(s.LOGO)
-time.sleep(1.5)
-gbf.play(f'{s.TFP}sounds{s.s}crack.wav')
-input(f"      __PRESS ENTER__"); gbf.play(f'{s.TFP}sounds{s.s}select.wav'); clear()
-input(t.TextBox(f"{s.markdown(1)}게임 설명{s.colors['end']}\nTextBox.Line\n\n↑, ←, ↓, →  -  화살표 키를 눌러 이동합니다.\n\n{s.p1}  -  당신입니다.\n{s.e}  -  방을 돌아다니는 몬스터입니다.\n{s.item}  -  아이템 상자입니다. 허기를 채워주고 체력을 1 또는 2를 회복시켜줍니다.\n{s.wall}  -  방의 기본 벽입니다. 벽에 부딪히면 방어력 또는 체력이 깎입니다.\n{s.floor}  -  빈 공간입니다. 자유롭게 드나들 수 있습니다.\n{s.goal}  -  다음 레벨로 이동하는 곳입니다. 도착 시 다음 레벨로 갈 수 있습니다.\n{s.R}  -  방을 드나들 수 있는 문입니다. 이동시 다른 방으로 갈 수 있습니다.\n\n{s.colors['R']}hp{s.colors['end']}  -  현재 당신의 체력입니다. 스테이지를 깰 때 마다 확률적으로 1씩 회복되며, 최대 체력을 늘릴 수도 있습니다.\n{s.colors['B']}def{s.colors['end']}  -  현재 당신의 방어력입니다. 스테이지를 깰 때 마다 1씩 회복되며, 최대 방어력을 늘릴 수도 있습니다.\n{s.colors['G']}atk{s.colors['end']}  -  당신의 공격력입니다. 스테이지를 깰 때 마다 공격력을 늘릴 수 있습니다.\n{s.colors['lY']}hunger{s.colors['end']}  -  얼마나 움직일 수 있는지 알려줍니다. 아이템 상자를 통해 회복할 수 있습니다.", Type="middle", fillChar=" ", outDistance=1, AMLS=True, endLineBreak=True)+"PRESS ENTER__")
-gbf.play(f'{s.TFP}sounds{s.s}select.wav'); clear()
-
-mnl.addListener()
+mainSettings.init()
 
 while s.main > 0:
     S.stage(s.stage)
