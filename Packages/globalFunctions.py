d6d5b9cfed540ffa68fdb457ff43bfc609aa8725
Daho0980 <0980daho@gmail.com>
2023-03-02 20:49:21 +0900

    Fix(globalFunctions) : 허기 표시 버그 수정
    • 이제 허기가 10% 이하가 되었을 때 % 게이지가 아닌 숫자로 표시함

--- Packages/globalFunctions.py
+++ Packages/globalFunctions.py
@@ -50,7 +50,7 @@ def asciiPrint():
     Display = ""
     Display += statusBarFormatPrint(s.hp, "hp", s.Mhp, space="     ")
     Display += statusBarFormatPrint(s.df, "def", s.Mdf, s.colors['B'], space="    ")
-    if math.ceil(((s.hunger/500)*10)) <= 1: hungerTag = s.colors['lY'] + str(s.hunger) + s.colors['end']
+    if math.ceil(((s.hunger/500)*10)) <= 50: hungerTag = s.colors['lY'] + str(s.hunger) + s.colors['end']
     else: hungerTag = f"{s.colors['lY']}{(s.hunger/500)*100:0.0f}%{s.colors['end']}"
     Display += statusBarFormatPrint(math.ceil(((s.hunger/500)*100)/10), "hunger", color=s.colors['lY'], tag=hungerTag)
     Display += statusBarFormatPrint(s.atk, "atk", color=s.colors['G'], space="    ")
