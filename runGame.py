9b867131f3b3fa318f95790cc9d7f2e169c3234f
Daho0980 <0980daho@gmail.com>
2023-10-28 16:03:55 +0900

    Fix: ModuleNotFoundError(No module named '_distutils_hack')에러 수정

--- runGame.py
+++ runGame.py
@@ -1,8 +1,7 @@
 import os
-from   Packages.lib.data import status
+from   Packages.lib.data import status as s
 
-s          = '/' if os.name == 'posix' else '\\'
-status.TFP = str(os.path.abspath(f'Packages'))+s
-
+s.TFP = str(os.path.abspath(f'Packages'))+{'posix':'/', 'nt':'\\'}[os.name]
+'/' if os.name == 'posix' else '\\'
 os.system("clear" if os.name == "posix" else "cls")
-exec(open(os.path.abspath(f'Packages{s}main.py'), encoding='utf8').read())
+import Packages.main
