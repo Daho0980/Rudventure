67cb5a7c448f1d5280efc84f227aa9b8471a4d30
Daho0980 <0980daho@gmail.com>
2024-01-01 14:44:32 +0900

    Feat: 게임 오버 이후에도 처음 화면으로 돌아오도록 수정

--- run_for_mac.sh
+++ run_for_mac.sh
@@ -1,6 +1,12 @@
-ABSPATH=$(cd "$(dirname "$0")"; pwd -P)
+#!/bin/bash
+# shellcheck disable=SC1091
 
-cd     ${ABSPATH}
-source bin/activate
+cd     "$(cd "$(dirname "$0")" && pwd -P)" || exit
+source "bin/activate"
 
-python3 runGame.py
+while true; do
+    if ! python3 runGame.py; then
+        echo "에러가 발생했습니다. 프로그램을 종료합니다."
+        break
+    fi
+done
